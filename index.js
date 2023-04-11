7f05399ffde91a2ae77c1041ddcae35b8ad8e1b4
VMarsola <vinicius.marasola2@gmail.com>
2023-04-11 19:07:35 -0300

    console

--- index.js
+++ index.js
@@ -1,62 +1,93 @@
-//runs for every item that is inside of the array
+//iterates every item of the array
 const runFor = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
-    return element;
+    console.log("runFor:", element);
   }
 };
 
 //runs the same for, but written in a simpler way, it could also run an object if you use the "in" instance intead of "of"
 const runMinFor = (arr) => {
   for (el of arr) {
-    return el;
+    console.log("runMinFor: ", el);
   }
 };
 
-// iterate through every item, like a for but returns a list
+// iterate through every item, but returns a list
 const runMap = (arr) => {
   arr.map((e) => {
+    console.log("runMap", e);
     return e;
   });
 };
 
 //merge 2 arrays
-const concatArr = (arr1, arr2) => arr1.concat(arr2);
+const concatArr = (arr1, arr2) => {
+  console.log("concatArr:", arr1.concat(arr2));
+  return arr1.concat(arr2);
+};
 
 //push an item to the last position
 const pushItem = (arr) => {
   arr.push("d");
-  console.log(arr);
+  console.log("pushItem", arr);
 };
 
 //remove the last item
 const doPop = (arr) => {
   arr.pop();
-  console.log(arr);
+  console.log("doPop", arr);
 };
 
 //remove from the 3rd item to the end
-const spliceDelete = (arr) => arr.splice(3, Number.MAX_VALUE);
+const spliceDelete = (arr) => {
+  console.log("spliceDelete>", arr.splice(3, Number.MAX_VALUE));
+  arr.splice(3, Number.MAX_VALUE);
+};
 
 //works similarly as splice, but modifies the original array
 // it can also recive a start and a ending pointo
-const slice = (arr) => arr.slice(2);
+const slice = (arr) => {
+  console.log("slice:", arr.slice(2));
+  return arr.slice(2);
+};
 
 // turn a array into a concated string
-const join = (arr) => arr.join();
-const joinWithNoVirgula = (arr) => arr.join("");
-const joinWithNoSomething = (arr) => arr.join("-");
+const join = (arr) => {
+  console.log("join:", arr.join());
+  return arr.join();
+};
+const joinWithNoVirgula = (arr) => {
+  console.log("joinWithNoVirgula:", arr.join(""));
+  return arr.join("");
+};
+const joinWithNoSomething = (arr) => {
+  console.log("joinWithNoSomething:", arr.join("-"));
+  return arr.join("-");
+};
 
 //every is able to recive a validation and return  true or false
 const isMinorTwentie = (cur) => cur < 20;
-const every = (arr) => arr.every(isMinorTwentie);
+const every = (arr) => {
+  console.log("every: ");
+  return arr.every(isMinorTwentie);
+};
 
 // same as every but return the corresponding values to your validation
-const filtered = (arr) => arr.filter((e) => e.length > 4);
+const filtered = (arr) => {
+  console.log(
+    "filtered: ",
+    arr.filter((e) => e.length > 4)
+  );
+  return arr.filter((e) => e.length > 4);
+};
 
 //find the index of the item that you are looking for (item, fromIndex)
 // case it doesnt find, returns -1
-const findIndex = (arr) => arr.indexOf("dog");
+const findIndex = (arr) => {
+  console.log("findIndex: ", arr.indexOf("dog"));
+  return arr.indexOf("dog");
+};
 
 //iterate through every item, but is able to get the past item also
 const reduce = (arr) =>
@@ -65,10 +96,19 @@ const reduce = (arr) =>
   }, 0);
 
 //finds the element to the given index
-const atExample = (arr) => arr.at(2);
+const atExample = (arr) => {
+  console.log(arr.at(2));
+  return arr.at(2);
+};
 
 //return a list of elements that pass the validation
-const find = (arr) => arr.find((e, i) => e > 89);
+const find = (arr) => {
+  console.log(arr.find((e, i) => e > 89));
+  return arr.find((e, i) => e > 89);
+};
 
-// checks whether an element is even
-const even = (element) => element % 2 === 0;
+// checks whether an element is even it returns 0 to true and 1 to false
+const even = (element) => {
+  console.log(element % 2 === 0);
+  return element % 2 === 0;
+};
